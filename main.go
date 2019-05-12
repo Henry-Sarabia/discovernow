ff83234aeb0e5937981b5992815868a9b3d84e82
Henry-Sarabia <henrysarabia@mail.fresnostate.edu>
2019-05-11 19:12:57 -0700

    Prevent playlist URI from being escaped by template parser

--- main.go
+++ main.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	"encoding/json"
+	"github.com/Henry-Sarabia/blank"
 	"github.com/Henry-Sarabia/discovernow/views"
+	"github.com/pkg/errors"
+	"html/template"
+	"io/ioutil"
 	"net/http"
+	"net/url"
 )
 
 const APIURL string = "http://127.0.0.1:8080/api/v1/"
@@ -39,110 +45,113 @@ func indexHandler(w http.ResponseWriter, r *http.Request) *serverError {
 		URL string `json:"url"`
 	}
 
-	//resp, err := http.Get(APIURL + "login")
-	//if err != nil {
-	//	return &serverError{
-	//		Error: err,
-	//		Message: "Cannot connect to login endpoint",
-	//		Code: http.StatusBadGateway,
-	//	}
-	//}
-	//defer resp.Body.Close()
-	//
-	//b, err := ioutil.ReadAll(resp.Body)
-	//if err != nil {
-	//	return &serverError{
-	//		Error: err,
-	//		Message: "Invalid response body from login endpoint",
-	//		Code: http.StatusInternalServerError,
-	//	}
-	//}
-	//
-	//log := &login{}
-	//
-	//err = json.Unmarshal(b, &log)
-	//if err != nil {
-	//	return &serverError{
-	//		Error: err,
-	//		Message: "Cannot unmarshal JSON response from login endpoint",
-	//		Code: http.StatusInternalServerError,
-	//	}
-	//}
+	resp, err := http.Get(APIURL + "login")
+	if err != nil {
+		return &serverError{
+			Error: err,
+			Message: "Cannot connect to login endpoint",
+			Code: http.StatusBadGateway,
+		}
+	}
+	defer resp.Body.Close()
 
-	dummy := login{URL: "http://127.0.0.1:3000/results"}
-	home.Render(w, dummy)
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return &serverError{
+			Error: err,
+			Message: "Invalid response body from login endpoint",
+			Code: http.StatusInternalServerError,
+		}
+	}
+
+	log := &login{}
+
+	err = json.Unmarshal(b, &log)
+	if err != nil {
+		return &serverError{
+			Error: err,
+			Message: "Cannot unmarshal JSON response from login endpoint",
+			Code: http.StatusInternalServerError,
+		}
+	}
+
+	home.Render(w, log)
+	//dummy := login{URL: "http://127.0.0.1:3000/results"}
+	//home.Render(w, dummy)
 	return nil
 }
 
 func resultsHandler(w http.ResponseWriter, r *http.Request) *serverError {
 	type playlist struct {
-		URI string `json:"uri"`
+		URI template.URL `json:"uri"`
 	}
 
-	//q := r.URL.Query()
-	//
-	//code := q.Get("code")
-	//if blank.Is(code) {
-	//	return &serverError{
-	//		Error: errors.New("results url is missing code"),
-	//		Message: "Results URL is missing code parameter",
-	//		Code: http.StatusInternalServerError,
-	//	}
-	//}
-	//
-	//state := q.Get("state")
-	//if blank.Is(state) {
-	//	return &serverError{
-	//		Error: errors.New("results url is missing state"),
-	//		Message: "Results URL is missing state parameter",
-	//		Code: http.StatusInternalServerError,
-	//	}
-	//}
-	//
-	//v := url.Values{}
-	//v.Set("code", code)
-	//v.Set("state", state)
-	//
-	//resp, err := http.Get(APIURL + "playlist" + "?" + v.Encode())
-	//if err != nil {
-	//	return &serverError{
-	//		Error: err,
-	//		Message: "Cannot connect to playlist endpoint",
-	//		Code: http.StatusBadGateway,
-	//	}
-	//}
-	//defer resp.Body.Close()
-	//
-	//b, err := ioutil.ReadAll(resp.Body)
-	//if err != nil {
-	//	return &serverError{
-	//		Error: err,
-	//		Message: "Invalid response body from playlist endpoint",
-	//		Code: http.StatusInternalServerError,
-	//	}
-	//}
-	//
-	//list := &playlist{}
-	//
-	//err = json.Unmarshal(b, &list)
-	//if err != nil {
-	//	return &serverError{
-	//		Error: err,
-	//		Message: "Cannot unmarshal JSON response from playlist endpoint",
-	//		Code: http.StatusInternalServerError,
-	//	}
-	//}
-	//
-	//if blank.Is(list.URI) {
-	//	return &serverError {
-	//		Error: err,
-	//		Message: "URI value is blank",
-	//		Code: http.StatusInternalServerError,
-	//	}
-	//}
+	q := r.URL.Query()
 
-	dummy := &playlist{URI: "something"}
-	results.Render(w, dummy)
+	code := q.Get("code")
+	if blank.Is(code) {
+		return &serverError{
+			Error: errors.New("results url is missing code"),
+			Message: "Results URL is missing code parameter",
+			Code: http.StatusInternalServerError,
+		}
+	}
+
+	state := q.Get("state")
+	if blank.Is(state) {
+		return &serverError{
+			Error: errors.New("results url is missing state"),
+			Message: "Results URL is missing state parameter",
+			Code: http.StatusInternalServerError,
+		}
+	}
+
+	v := url.Values{}
+	v.Set("code", code)
+	v.Set("state", state)
+
+	resp, err := http.Get(APIURL + "playlist" + "?" + v.Encode())
+	if err != nil {
+		return &serverError{
+			Error: err,
+			Message: "Cannot connect to playlist endpoint",
+			Code: http.StatusBadGateway,
+		}
+	}
+	defer resp.Body.Close()
+
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return &serverError{
+			Error: err,
+			Message: "Invalid response body from playlist endpoint",
+			Code: http.StatusInternalServerError,
+		}
+	}
+
+	list := &playlist{}
+
+	err = json.Unmarshal(b, &list)
+	if err != nil {
+		return &serverError{
+			Error: err,
+			Message: "Cannot unmarshal JSON response from playlist endpoint",
+			Code: http.StatusInternalServerError,
+		}
+	}
+
+	if blank.Is(string(list.URI)) {
+		return &serverError {
+			Error: err,
+			Message: "URI value is blank",
+			Code: http.StatusInternalServerError,
+		}
+	}
+
+	results.Render(w, list)
+
+	//dummy := &playlist{URI: "something"}
+	//results.Render(w, dummy)
 	return nil
 }
 
