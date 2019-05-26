890db5f051410e87ab91d938d1ad36ca6d7e19a7
Henry-Sarabia <henrysarabia@mail.fresnostate.edu>
2019-05-26 12:32:41 -0700

    Replace javascript smooth scroll with anchor + css smooth scroll

--- static/js/scroll.js
+++
@@ -1,5 +0,0 @@
-function smoothScroll(id) {
-    requestAnimationFrame(function() {
-        document.getElementById(id).scrollIntoView({behavior: "smooth", block: "start", inline: "start"});
-    })
-}
